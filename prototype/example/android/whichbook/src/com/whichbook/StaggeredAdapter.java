a3c21f432adba6f1435cd062adc0db0bf98a8c9e
i5on9i <gaedduck@gmail.com>
2013-06-12 11:02:11 +0900

    - add android grid view template source - arrange the example folder

---
+++ prototype/example/android/whichbook/src/com/whichbook/StaggeredAdapter.java
@@ -0,0 +1,50 @@
+package com.whichbook;
+
+
+
+import android.content.Context;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+
+import com.lib.loader.ImageLoader;
+import com.lib.views.ScaleImageView;
+
+public class StaggeredAdapter extends ArrayAdapter<String> {
+
+	private ImageLoader mLoader;
+
+	public StaggeredAdapter(Context context, int textViewResourceId,
+			String[] objects) {
+		super(context, textViewResourceId, objects);
+		mLoader = new ImageLoader(context);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		ViewHolder holder;
+
+		if (convertView == null) {
+			LayoutInflater layoutInflator = LayoutInflater.from(getContext());
+			convertView = layoutInflator.inflate(R.layout.row_staggered_demo,
+					null);
+			holder = new ViewHolder();
+			holder.imageView = (ScaleImageView) convertView .findViewById(R.id.imageView1);
+			convertView.setTag(holder);
+		}
+
+		holder = (ViewHolder) convertView.getTag();
+
+		mLoader.DisplayImage(getItem(position), holder.imageView);
+
+		return convertView;
+	}
+
+	static class ViewHolder {
+		ScaleImageView imageView;
+	}
+}
